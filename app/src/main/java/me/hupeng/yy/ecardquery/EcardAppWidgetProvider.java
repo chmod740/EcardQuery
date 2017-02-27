8bb14997ffeca9497257fdcdb45755c1d7d73c72
HUPENG <hupeng@imudges.com>
2017-02-27 13:38:54 +0800

    添加部件类

---
+++ app/src/main/java/me/hupeng/yy/ecardquery/EcardAppWidgetProvider.java
@@ -0,0 +1,10 @@
+package me.hupeng.yy.ecardquery;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by HUPENG on 2017/2/27.
+ */
+public class EcardAppWidgetProvider extends AppWidgetProvider {
+
+}
